7140cfb9a2faa99e57304534e1c32815c9253060
JesHardyM <jcondottiere@gmail.com>
2023-10-10 09:36:19 +0200

    update models

--- server/models/ProductModel.js
+++ server/models/ProductModel.js
@@ -3,7 +3,7 @@ import { DataTypes } from 'sequelize';
 
 const ProductModel = db.define("products", {
     product_name:{type:DataTypes.STRING}, 
-    brand_id:{type:DataTypes.INTEGER}, 
+    artist_id:{type:DataTypes.INTEGER}, 
     product_description:{type:DataTypes.STRING}, //schema varchar
     category:{type:DataTypes.STRING},
     price:{type:DataTypes.FLOAT},
