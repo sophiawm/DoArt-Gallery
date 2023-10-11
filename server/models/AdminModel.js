9acd9923c4f96404e6ecddc480990236eabf39ce
JesHardyM <jcondottiere@gmail.com>
2023-10-11 12:47:26 +0200

    start UUID

--- server/models/AdminModel.js
+++ server/models/AdminModel.js
@@ -1,7 +1,10 @@
 import db from '../database/db.js';
-import { DataTypes } from 'sequelize';
+import { DataTypes, UUIDV4 } from 'sequelize';
 
 const AdminModel = db.define("admins", {
+    id: {type:DataTypes.UUID,
+        defaultValue: UUIDV4,
+        primaryKey: true,},
     user_name:{type:DataTypes.STRING}, 
     user_email:{type:DataTypes.STRING}, 
     user_password:{type:DataTypes.STRING},
