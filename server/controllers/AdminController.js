f1d9a177e591f642ffb5faa2e6e97d7ce50ad35f
JesHardyM <jcondottiere@gmail.com>
2023-10-20 15:02:55 +0200

    add attempt at createProduct.jsx

--- server/controllers/AdminController.js
+++ server/controllers/AdminController.js
@@ -7,7 +7,7 @@ export const createAdmin = async (req, res) => {
         await AdminModel.create(req.body)
         res.status(200).json({message: "This administrator has been added successfully!"})
     }catch (error){
-        res.status(500).json({message: error.errors})
+        res.status(500).json({message: error})
     }
 }
 
